3ffeda486ae08831a4205df43b292ff3c2a6a01a
virtu-alex <alessio.cordari@gmail.com>
2022-09-27 14:23:06 +0200

    feat: migration + seeding ok

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +12,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_post = new Post();
+            $new_post->title = $faker->text(20);
+            $new_post->slug = Str::slug($new_post->title, '-');
+            $new_post->content = $faker->paragraphs(2, true);
+            $new_post->image = $faker->imageUrl(250, 250);
+
+            $new_post->save();
+        }
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $user = new User();
+        // $user->name = 'virtu-alex';
+        // $user->email = 'alessio.cordari@gmail.com';
+        // $user->password = bcrypt('computer94');
+        // $user->save();
+    }
+}
