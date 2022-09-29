667ed2a03138bb88efd9fb9b0cac5222656819be
virtu-alex <alessio.cordari@gmail.com>
2022-09-29 16:43:42 +0200

    feat: Add select filter on index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,12 +5,27 @@
         <h1>Lista Post</h1>
         <a class="btn btn-success" href="{{ route('admin.posts.create') }}">
             <i class="fa-solid fa-plus my-2"></i> Nuovo Post</a>
+            <form action='' method="">
+                <div class="input-group">
+                    <select name="category_id" id="custom-select">
+                        <option value="">Tutte le categorie</option>
+                        @foreach ($categories as $category)
+                            <option @if ($category->id == $selected_category) selected @endif value={{ $category->id }}>
+                                {{ $category->label }} </option>
+                        @endforeach
+                    </select>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit">Filtra</button>
+                    </div>
+                </div>
+            </form>
     </header>
     <table class="table table-striped table-dark">
         <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Titolo</th>
+                <th scope="col">Autore</th>
                 <th scope="col">Categoria</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Creato il </th>
@@ -24,6 +39,13 @@
                     <th scope="row">{{ $post->id }}</th>
                     <td>{{ $post->title }}</td>
                     <td>
+                        @if ($post->user)
+                            {{ $post->user->name }}
+                        @else
+                            Autore anonimo
+                        @endif
+                    </td>
+                    <td>
                         @if ($post->category)
                             <span class="p-2 badge badge-{{ $post->category->color }}">{{ $post->category->label }}</span>
                         @else
@@ -35,16 +57,16 @@
                     <td>{{ $post->updated_at }}</td>
                     <td>
                         <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST" class="delete-form">
-
                             <a class="btn btn-sm btn-primary" href="{{ route('admin.posts.show', $post) }}"><i
-                                    class="fa-solid fa-eye"></i> Mostra</a>
-                            <a class="btn btn-sm btn-warning" href="{{ route('admin.posts.edit', $post) }}"><i
-                                    class="fa-solid fa-pencil"></i> Modifica</a>
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-sm btn-danger" type="submit">
-                                <i class="fa-solid fa-trash"></i>Elimina
-                            </button>
+                                    class="fa-solid fa-eye"></i> </a>
+                            {{-- @if ($post->user_id === Auth::id()) --}}
+                                <a class="btn btn-sm btn-warning" href="{{ route('admin.posts.edit', $post) }}"><i
+                                        class="fa-solid fa-pencil"></i></a>
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-sm btn-danger" type="submit">
+                                    <i class="fa-solid fa-trash"></i></button>
+                            {{-- @endif --}}
                         </form>
 
                     </td>
@@ -52,7 +74,7 @@
                 </tr>
             @empty
                 <tr>
-                    <td colspan="6">
+                    <td colspan="7">
                         <h3 class="text-center">Nessun Post</h3>
                     </td>
                 </tr>
