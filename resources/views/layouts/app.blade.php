48051da733b41e496b65542f2459f603c417dd9a
virtu-alex <alessio.cordari@gmail.com>
2022-09-29 16:35:27 +0200

    feat: Add user section in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,10 @@
                             <a class="nav-link @if (Route::is('admin.posts.index')) active @endif"
                                 href="{{ route('admin.posts.index') }}">Posts</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link @if (Route::is('admin.users.index')) active @endif"
+                                href="{{ route('admin.users.index') }}">Utenti</a>
+                        </li>
                     </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
