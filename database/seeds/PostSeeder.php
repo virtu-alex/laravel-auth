08d3c0bb1e50f487cbc857805cc729be40bdc3a2
virtu-alex <alessio.cordari@gmail.com>
2022-09-30 18:25:41 +0200

    feat: trying to show tags on index

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use Illuminate\Support\Arr;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\Category;
 use App\User;
 
@@ -18,6 +19,7 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
         $user_ids = User::pluck('id')->toArray();
+        $tag_ids = Tag::pluck('id')->toArray();
         $category_ids = Category::pluck('id')->toArray();
         for ($i = 0; $i < 10; $i++) {
             $new_post = new Post();
@@ -28,6 +30,13 @@ class PostSeeder extends Seeder
             $new_post->content = $faker->paragraphs(2, true);
             $new_post->image = $faker->imageUrl(250, 250);
             $new_post->save();
+            $post_tags = [];
+
+            foreach ($tag_ids as $tag_id) {
+                if ($faker->boolean()) $post_tags[] = $tag_id;
+            }
+
+            $new_post->tags()->attach($post_tags);
         }
     }
 }
